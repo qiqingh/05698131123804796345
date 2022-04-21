78b2ef5a3af04e848a15bde345fc5d4826044b87
qiqingh <62452864+qiqingh@users.noreply.github.com>
2022-04-20 21:23:25 -0400

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,12 +1,12 @@
-import os 
+#import os 
 
 
-fileName = []
-with open("./process.txt") as readFile:
-	for line in readFile:
-		line = line.strip()
-		if(line not in fileName):
-			fileName.append(line)
+#fileName = []
+#with open("./process.txt") as readFile:
+#	for line in readFile:
+#		line = line.strip()
+#		if(line not in fileName):
+#			fileName.append(line)
 
-for name in fileName:
-	os.system("cp -r /home/qiqingh/Desktop/virtual-honeypot/virtual_openwrt/" + name + " /home/qiqingh/Desktop/virtual-honeypot/simplifiedVirtualWrt/" + name)
+#for name in fileName:
+#	os.system("cp -r /home/qiqingh/Desktop/virtual-honeypot/virtual_openwrt/" + name + " /home/qiqingh/Desktop/virtual-honeypot/simplifiedVirtualWrt/" + name)
